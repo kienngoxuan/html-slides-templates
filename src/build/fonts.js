720bf459ab7a582904ad470a73bb026002757739
Sotatek-KienNgo <kien.ngo@sotatek.com>
2026-05-25 21:30:57 +0700

    build: add FONT_FILES list for self-hosted WOFF2 fonts (Inter, Outfit, JetBrains Mono)

---
+++ src/build/fonts.js
@@ -0,0 +1,17 @@
+const FONT_FILES = [
+  { family: 'Inter', weight: 400, style: 'normal', file: 'src/fonts/inter-latin-400-normal.woff2' },
+  { family: 'Inter', weight: 500, style: 'normal', file: 'src/fonts/inter-latin-500-normal.woff2' },
+  { family: 'Inter', weight: 600, style: 'normal', file: 'src/fonts/inter-latin-600-normal.woff2' },
+  { family: 'Inter', weight: 700, style: 'normal', file: 'src/fonts/inter-latin-700-normal.woff2' },
+  { family: 'JetBrains Mono', weight: 400, style: 'normal', file: 'src/fonts/jetbrains-mono-latin-400-normal.woff2' },
+  { family: 'JetBrains Mono', weight: 500, style: 'normal', file: 'src/fonts/jetbrains-mono-latin-500-normal.woff2' },
+  { family: 'Outfit', weight: 400, style: 'normal', file: 'src/fonts/outfit-latin-400-normal.woff2' },
+  { family: 'Outfit', weight: 500, style: 'normal', file: 'src/fonts/outfit-latin-500-normal.woff2' },
+  { family: 'Outfit', weight: 600, style: 'normal', file: 'src/fonts/outfit-latin-600-normal.woff2' },
+  { family: 'Outfit', weight: 700, style: 'normal', file: 'src/fonts/outfit-latin-700-normal.woff2' },
+  { family: 'Outfit', weight: 800, style: 'normal', file: 'src/fonts/outfit-latin-800-normal.woff2' },
+];
+
+module.exports = {
+  FONT_FILES,
+};
